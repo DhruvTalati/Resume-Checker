1f43e58da1ad3e8ba17645e3ef17386934ba05d3
Dhruv Talati <dhruvtalati640@gmail.com>
2026-06-19 16:46:13 +0530

    Frontend updated

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,9 +1,5 @@
-import axios from "axios";
+import api from "../../../api/axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials: true,
-});
 
 /**
  * @description Service to generate interview report based on user self description, resume and job description.
